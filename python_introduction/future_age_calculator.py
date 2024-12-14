faa50c19862f29f5fce312cd14e95a82cf44be53
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2024-12-15 00:03:45 +0100

    Task 4: user input age calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+current_age = int(input("How old are you? "))
+current_year = 2023
+future_year = 2050
+future_age = future_year - current_year + current_age
+print("In " + str(future_year) + ", you will be " + str(future_age) + " years old.")
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,4 @@
 hours = 2
-seconds_in_a_minute = 60
-seconds_in_an_hour = seconds_in_a_minute * 60
-total_seconds = hours * seconds_in_an_hour
-print(hours, "hour(s)", "is", total_seconds, "seconds")
+seconds = hours * 3600
+print(hours, "hour(s)", "is", seconds, "seconds")
 
