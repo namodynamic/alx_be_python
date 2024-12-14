8bf75377d191dcabf2657de78581c6e585d801a0
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2024-12-14 23:31:16 +0100

    Task 3: convert hours to seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+hours = 2
+seconds_in_a_minute = 60
+seconds_in_an_hour = seconds_in_a_minute * 60
+total_seconds = hours * seconds_in_an_hour
+print(hours, "hour(s)", "is", total_seconds, "seconds")
+
