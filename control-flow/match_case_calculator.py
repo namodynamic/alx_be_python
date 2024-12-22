16e5c0f2e6a363b4999bd137d16f09cb3fbb1508
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2024-12-22 07:36:29 +0100

    Modify to match statement for operations

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,22 +1,26 @@
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-operations = input("Choose the operation (+, -, *, /): ")
+def calculator():
+   num1 = float(input("Enter the first number: "))
+   num2 = float(input("Enter the second number: "))
+   operations = input("Choose the operation (+, -, *, /): ")
 
-match operations:
-    case "+":
-        result = num1 + num2
-    case "-":
-        result = num1 - num2
-    case "*":
-        result = num1 * num2
-    case "/":
-        if num2 == 0:
-            print("Cannot divide by zero.")
-            exit()
-        else:
-            result = num1 / num2
-    case _:
-        print("Invalid operator")
-        exit()
+   match operations:
+       case "+":
+           result = num1 + num2
+       case "-":
+           result = num1 - num2
+       case "*":
+           result = num1 * num2
+       case "/":
+           if num2 == 0:
+               print("Cannot divide by zero.")
+               return
+           else:
+               result = num1 / num2
+       case _:
+           print("Invalid operator")
+           return
 
-print(f"The result is {result}")
+   print(f"The result is {result}")
+
+calculator()
+
