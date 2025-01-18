9184ca2ea26787754599bf28f14c1ac435c38dd4
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-01-18 14:53:18 +0100

    Enhance bank account withdrawal logic and improve balance display formatting

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,16 +1,19 @@
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance
+    def __init__(self, account_balance=0):
+        self.__account_balance = account_balance
 
     def deposit(self, amount):
         self.__account_balance += amount
 
     def withdraw(self, amount):
         if amount > 0 and amount <= self.__account_balance:
-            self.__account_balance -= amount
-            return True
+           self.__account_balance -= amount
+           return True
+        elif amount > self.__account_balance:
+            return False
         else:
             return False
+     
 
-    def display_balance(self):
-        print(f"Current balance: ${self.__account_balance}")
+    def display_balance(self):  
+        print(f"Current Balance: ${self.__account_balance}")
